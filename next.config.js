ca1370456095b6fa9abb58062de90bc8a28047d7
ZEN EX <zenex133@gmail.com>
2026-04-01 21:59:55 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
+  experimental: {
+    allowedDevOrigins: ['6000-firebase-ipos-1-1775045772528.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev'],
+  },
 }
 
 module.exports = nextConfig
