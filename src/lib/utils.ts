955464a89c7aeaf8c4f2b5967a2a4c2280573518
ZEN EX <zenex133@gmail.com>
2026-04-03 16:43:19 +0000

    قم بمراجعة التطبيق بدقة و عمق شديدين و اعطني جميع الاكواد الميتة و قم بح

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
-import type { Cart, Product, SaleItem } from "./types";
+import type { Product } from "./types";
  
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
